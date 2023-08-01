f7fe6234618dc701bcf898a7d10ef7a4cca7d9fc
Jeethula <jeethula10@gmail.com>
2023-08-01 09:55:54 +0530

    upadted transport

--- src/Transport/TransportWrapper.jsx
+++ src/Transport/TransportWrapper.jsx
@@ -3,9 +3,17 @@ import TransportContainer from './TransportContainer'
 
 function TransportWrapper() {
   return (
+    <>
+    <header> 
+      <h1 style={{color: "blue",backgroundColor:"orange",textAlign:"center", fontSize:"3rem"}} >Transportation Registration form</h1>
+    </header>
     <div className='flex justify-center items-center h-screen bg-[#1976d2]'>
       <TransportContainer/>
     </div>
+    <footer style={{color: "blue",backgroundColor:"orange",textAlign:"center", fontSize:"1rem"}}>
+        for any querys contact shang2sree@gmail.com
+    </footer>
+    </>
   )
 }
 
