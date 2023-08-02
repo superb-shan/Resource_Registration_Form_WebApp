4f27674e244656893e596ee4a09022e4231dd1d3
ragunandhant <raja.g2020cse@sece.ac.in>
2023-08-02 16:23:18 +0530

    transport

--- src/Admin/AdminViewTable.jsx
+++ src/Admin/AdminViewTable.jsx
@@ -11,9 +11,9 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Stack from '@mui/material/Stack';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {IoCloseCircleOutline } from "react-icons/io5";
+import { IoCloseCircleOutline } from "react-icons/io5";
 import axios from 'axios';
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 
 const theme = createTheme({
@@ -41,41 +41,40 @@ function AdminViewTable() {
 
   const [isOpen, setIsOpen] = React.useState(false);
   const [selectedRow, setSelectedRow] = React.useState(null);
-  const[isLoading,setisloading]=useState(true)
-  const [userData,setuserData]=useState([])
+  const [isLoading, setisloading] = useState(true)
+  const [userData, setuserData] = useState([])
 
-  const fetechData= async ()=>{
-    try{
-      const response= await axios.get('http://localhost:8000/transport/get',{params:{name:'g2'}})
+  const fetechData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/transport/get')
       console.log(response.data.data)
       setuserData(response.data.data)
       setisloading(false)
     }
-      catch(error)
-      {
-        console.log("Error", error)
-      };
+    catch (error) {
+      console.log("Error", error)
+    };
 
   }
 
 
-  const accept = async(id)=>{
-    const res = await axios.patch('http://localhost:8000/transport/update',{id,isapproved:'true'})
+  const accept = async (id) => {
+    const res = await axios.patch('http://localhost:8000/transport/update', { id, isapproved: 'true' })
     console.log(res)
     fetechData()
     handleClose()
   }
-  const reject = async(id)=>{
-    const res = await axios.patch('http://localhost:8000/transport/update',{id,isapproved:'false'})
+  const reject = async (id) => {
+    const res = await axios.patch('http://localhost:8000/transport/update', { id, isapproved: 'false' })
     console.log(res)
     fetechData()
     handleClose()
   }
-  useEffect(()=>{
-    
+  useEffect(() => {
+
     fetechData()
-  
-  },[])
+
+  }, [])
 
 
   const handleOpen = (rowData) => {
@@ -95,7 +94,7 @@ function AdminViewTable() {
         headerName: 'Actions',
         width: 150,
         renderCell: (params) => (
-          <Button variant="contained"  onClick={() => handleOpen(params.row)}>
+          <Button variant="contained" onClick={() => handleOpen(params.row)}>
             Show more
           </Button>
         ),
@@ -108,95 +107,95 @@ function AdminViewTable() {
       width: 200,
     };
   });
-    if(isLoading){
-      return <div>Loading...</div>
-    }
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
   return (
     <ThemeProvider theme={theme}>
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={userData}
-        columns={columns}
-        components={{
-          Toolbar: GridToolbar,
-        }}
-      />
-      <Modal open={isOpen} onClose={handleClose}>
-        <div>
-          {/* Render the detailed information from selectedRow */}
-          {selectedRow && (
-            <div>
-              <Box sx={style}>
-                <div style={{textAlign:'right'}}>
-    <Button onClick={handleClose}  ><IoCloseCircleOutline/></Button>
-    </div>
-          <Typography id="modal-modal-title" variant="h6" component="h2" style={{textAlign:'center'}}>
-            Details
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <TableContainer>
-                <Table>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>Type</TableCell>
-                      <TableCell>{selectedRow.type}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Name</TableCell>
-                      <TableCell>{selectedRow.name}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Phone Number</TableCell>
-                      <TableCell>{selectedRow.number}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Purpose</TableCell>
-                      <TableCell>{selectedRow.purpose}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Date</TableCell>
-                      <TableCell>{selectedRow.date}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Time</TableCell>
-                      <TableCell>{selectedRow.time}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>PickUp Location</TableCell>
-                      <TableCell>{selectedRow.pickUp}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Drop Location</TableCell>
-                      <TableCell>{selectedRow.drop}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>No of Passengers</TableCell>
-                      <TableCell>{selectedRow.passengerCount}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Special Requirement</TableCell>
-                      <TableCell>{selectedRow.specialRequirements}</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-                </TableContainer>
-          </Typography>
-          <Stack direction="row" style={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '10px' }}>
-  <Button variant="contained" color="success" disabled={selectedRow.isapproved !== null} onClick={()=>{accept(selectedRow.id)}}>
-    Accept
-  </Button>
-  <Button variant="contained" color="error"  disabled={selectedRow.isapproved !== null} onClick={()=>{reject(selectedRow.id)}}>
-    Reject
-  </Button>
-</Stack>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={userData}
+          columns={columns}
+          components={{
+            Toolbar: GridToolbar,
+          }}
+        />
+        <Modal open={isOpen} onClose={handleClose}>
+          <div>
+            {/* Render the detailed information from selectedRow */}
+            {selectedRow && (
+              <div>
+                <Box sx={style}>
+                  <div style={{ textAlign: 'right' }}>
+                    <Button onClick={handleClose}  ><IoCloseCircleOutline /></Button>
+                  </div>
+                  <Typography id="modal-modal-title" variant="h6" component="h2" style={{ textAlign: 'center' }}>
+                    Details
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <TableContainer>
+                      <Table>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell>Type</TableCell>
+                            <TableCell>{selectedRow.type}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>{selectedRow.name}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>Phone Number</TableCell>
+                            <TableCell>{selectedRow.number}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>Purpose</TableCell>
+                            <TableCell>{selectedRow.purpose}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>Date</TableCell>
+                            <TableCell>{selectedRow.date}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>Time</TableCell>
+                            <TableCell>{selectedRow.time}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>PickUp Location</TableCell>
+                            <TableCell>{selectedRow.pickUp}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>Drop Location</TableCell>
+                            <TableCell>{selectedRow.drop}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>No of Passengers</TableCell>
+                            <TableCell>{selectedRow.passengerCount}</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>Special Requirement</TableCell>
+                            <TableCell>{selectedRow.specialRequirements}</TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Typography>
+                  <Stack direction="row" style={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '10px' }}>
+                    <Button variant="contained" color="success" disabled={selectedRow.isapproved !== null} onClick={() => { accept(selectedRow.id) }}>
+                      Accept
+                    </Button>
+                    <Button variant="contained" color="error" disabled={selectedRow.isapproved !== null} onClick={() => { reject(selectedRow.id) }}>
+                      Reject
+                    </Button>
+                  </Stack>
 
-   
-          </Box>
-            </div>
-       )}
-        </div>
-      </Modal>  
-    </div>
+
+                </Box>
+              </div>
+            )}
+          </div>
+        </Modal>
+      </div>
     </ThemeProvider>
   );
 }
