b66d25e254c1cf74f2342e724af719ed35fee42e
ragunandhant <raja.g2020cse@sece.ac.in>
2023-09-14 09:43:48 +0530

    changes

--- src/Context/Seminar.Context.jsx
+++ src/Context/Seminar.Context.jsx
@@ -13,31 +13,43 @@ const SeminarProvider = ({ children }) => {
     { name: 'Board Room', maxCapacity: 15 },
     { name: 'Ignite', maxCapacity: 15 },
     { name: 'GF-07', maxCapacity: 15 },
-    { name: 'Placement Lab', maxCapacity: 15 },
+    { name: 'Code Studio', maxCapacity: 15 },
     { name: 'IT center', maxCapacity: 15 },
-    { name: 'Seminar Hall 1st Floor', maxCapacity: 15 },
-    { name: 'Seminar Hall 2nd Floor', maxCapacity: 15 },
-    { name: 'Others', maxCapacity: 15 }
-  ]
-  ;
+    { name: 'Auditorium 1st Floor', maxCapacity: 15 },
+    { name: 'Auditorium Hall 2nd Floor', maxCapacity: 15 },
+    { name: '3rd Floor Drawing Hall', maxCapacity: 15 },
+    { name: 'Maker Space', maxCapacity: 15 },
+    { name: 'AI Robo Space', maxCapacity: 15 },
+    { name: 'Bytes Lab', maxCapacity: 15 },
+    { name: 'CCE Lab', maxCapacity: 15 },
+    { name: 'AIDS Lab', maxCapacity: 15 },
+    { name: 'CSBS Lab', maxCapacity: 15 },
+    { name: 'Fullstack Lab', maxCapacity: 15 },
+    { name: 'ECE Lab', maxCapacity: 15 },
+    { name: '2nd Floor project Lab', maxCapacity: 15 },
+    { name: 'CAD Lab', maxCapacity: 15 },
 
-    return (
-      <SeminarContext.Provider
-        value={{
-            //new 
-            isAvailabilityChecked, 
-            setIsAvailabilityChecked,
-            unavailableHalls, 
-            setUnavailableHalls,
-            startDateTime, setStartDateTime,
-            endDateTime, setEndDateTime,
-            allHalls,
-        }}
-      >
-        {children}
-      </SeminarContext.Provider>
-    );
-  };
-  
-  export default SeminarProvider;
-  
+
+
+  ]
+    ;
+
+  return (
+    <SeminarContext.Provider
+      value={{
+        //new 
+        isAvailabilityChecked,
+        setIsAvailabilityChecked,
+        unavailableHalls,
+        setUnavailableHalls,
+        startDateTime, setStartDateTime,
+        endDateTime, setEndDateTime,
+        allHalls,
+      }}
+    >
+      {children}
+    </SeminarContext.Provider>
+  );
+};
+
+export default SeminarProvider;
