7476fd8b52be4e67165439cfc4d2e3f03d038a6d
ragunandhant <raja.g2020cse@sece.ac.in>
2023-09-21 16:01:23 +0530

    calender

--- src/Components/Calender/CalendarView.jsx
+++ src/Components/Calender/CalendarView.jsx
@@ -8,7 +8,7 @@ import { Selector } from '../Fields/InteractionFields';
 import CalendarContainer from '../Containers/CalendarContainer';
 import UserDataModal from '../Modals/UserDataModal';
 import AdminDataModal from '../Modals/AdminDataModal';
-
+import './bigCustomStyles.css'
 const localizer = momentLocalizer(moment);
 
 const CalendarView = (props) => {
@@ -32,142 +32,153 @@ const CalendarView = (props) => {
   const fetchData = async (user, userName, formType) => {
     setIsLoading(true)
     console.log("formtype", formType)
-    
+
     const param = {};
     if (user === "user") param["name"] = userName;
     param["isapproved"] = true
     try {
-        
+
       let Data = [];
-        let seminarResponse = await axios.get(`/${formType === 'Seminar'? "seminar" : "guesthouse"}/get`, { params: param });
-        seminarResponse = seminarResponse.data.data;
-        console.log(seminarResponse);
-        console.log("events", events);
+      let seminarResponse = await axios.get(`/${formType === 'Seminar' ? "seminar" : "guesthouse"}/get`, { params: param });
+      seminarResponse = seminarResponse.data.data;
+      console.log(seminarResponse);
+      console.log("events", events);
 
-        if (seminarResponse.length > 0) {
-          Data = seminarResponse.map((event) => {
-            // Generate a random background color
-            let color = generateRandomColors()
-            // const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-            
-            // // Calculate text color for better contrast
-            // const textColor = getContrastColor(randomColor);
+      if (seminarResponse.length > 0) {
+        Data = seminarResponse.map((event) => {
+          // Generate a random background color
+          let color = generateRandomColors()
+          // const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-            return {
-              title: formType === 'Seminar' ? event.hallRequired : event.roomRequired,
-              start: new Date(formType === 'Seminar' ? Date.parse(event.startDateTime): moment(event.startDateTime, "DD-MM-YYYY HH:mm:ss").format("YYYY MM DD HH:mm:ss")),
-              end: new Date(formType === 'Seminar' ? Date.parse(event.endDateTime): moment(event.endDateTime, "DD-MM-YYYY HH:mm:ss").format("YYYY MM DD HH:mm:ss")),
-              colorEvento: color.backgroundColor,
-              val:event,
-              color: color.textColor,
-            };
-          });
-        }
-        console.log(Data, 'data');
-       // setIsLoading(false)
-        return Data;
+          // // Calculate text color for better contrast
+          // const textColor = getContrastColor(randomColor);
+
+          return {
+            title: formType === 'Seminar' ? `${event.hallRequired} (${moment(event.startDateTime).format('hh:mm A')} - ${moment(event.endDateTime).format('hh:mm A')})`: `${event.roomRequired} (${formatTime(event.startDateTime,"DD-MM-YYYY HH:mm:ss")} - ${formatTime(event.endDateTime,"DD-MM-YYYY HH:mm:ss")})`,
+            start: new Date(formType === 'Seminar' ? Date.parse(event.startDateTime) : moment(event.startDateTime, "DD-MM-YYYY HH:mm:ss").format("YYYY MM DD HH:mm:ss")),
+            end: new Date(formType === 'Seminar' ? Date.parse(event.endDateTime) : moment(event.endDateTime, "DD-MM-YYYY HH:mm:ss").format("YYYY MM DD HH:mm:ss")),
+            colorEvento: color.backgroundColor,
+            val: event,
+            color: color.textColor,
+          };
+        });
+      }
+      console.log(Data, 'data');
+      // setIsLoading(false)
+      return Data;
 
     } catch (error) {
       console.log("Error", error);
-    } 
+    }
     setIsLoading(false)
   };
   function generateRandomColors() {
-   
-  
-   
-      // Generate random RGB values
-      const r = Math.floor(Math.random() * 256);
-      const g = Math.floor(Math.random() * 256);
-      const b = Math.floor(Math.random() * 256);
-  
-      // Calculate brightness of the randomized color
-      const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-  
-      // Calculate brightness of white and black text
-      const lightText = (255 * 299 + 255 * 587 + 255 * 114) / 1000;
-      const darkText = (0 * 299 + 0 * 587 + 0 * 114) / 1000;
-  
-      // Determine contrast of color for text
-      const textColor = Math.abs(brightness - lightText) > Math.abs(brightness - darkText) ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)';
-  
-      return({
-        backgroundColor: `rgb(${r}, ${g}, ${b})`,
-        textColor: textColor,
-      });
-    
-  
-    
+
+
+
+    // Generate random RGB values
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    // Calculate brightness of the randomized color
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+    // Calculate brightness of white and black text
+    const lightText = (255 * 299 + 255 * 587 + 255 * 114) / 1000;
+    const darkText = (0 * 299 + 0 * 587 + 0 * 114) / 1000;
+
+    // Determine contrast of color for text
+    const textColor = Math.abs(brightness - lightText) > Math.abs(brightness - darkText) ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)';
+
+    return ({
+      backgroundColor: `rgb(${r}, ${g}, ${b})`,
+      textColor: textColor,
+    });
+
+
+
   }
 
   // Function to calculate text color based on background color
-//   const getContrastColor = (hexColor) => {
-//     const r = parseInt(hexColor.slice(1, 3), 16);
-//     const g = parseInt(hexColor.slice(3, 5), 16);
-//     const b = parseInt(hexColor.slice(5, 7), 16);
-//     const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+  //   const getContrastColor = (hexColor) => {
+  //     const r = parseInt(hexColor.slice(1, 3), 16);
+  //     const g = parseInt(hexColor.slice(3, 5), 16);
+  //     const b = parseInt(hexColor.slice(5, 7), 16);
+  //     const brightness = (r * 299 + g * 587 + b * 114) / 1000;
 
-//     return brightness > 128 ? 'black' : 'white';
-//   };
-//  setInterval(()=>{fetchData(user, userName,formType).then((data) => {
-//     console.log(data);
-//     setEvent(data);
-//   });},1000*60)
+  //     return brightness > 128 ? 'black' : 'white';
+  //   };
+  //  setInterval(()=>{fetchData(user, userName,formType).then((data) => {
+  //     console.log(data);
+  //     setEvent(data);
+  //   });},1000*60)
 
   useEffect(() => {
     console.log(formType)
-    
+
     if (isLoading) {
-      fetchData(user, userName,formType).then((data) => {
+      fetchData(user, userName, formType).then((data) => {
         console.log(data);
         setEvent(data);
       });
     }
-    
+
   }, [formType]);
+  function formatTime(dateTime,format ) {
+    return moment(dateTime,format).format('hh:mm A'); // Customize the format as needed
+  }
 
   return (
-    <div className='flex flex-col items-center p-5 gap-5'>
-        <div className='bg-white p-1 rounded-[5px]'>
-            <Selector value={formType} setValue={setFormType} list={[{name: "Seminar"}, {name: "Guest House"}]}/>
-        </div>
-        <CalendarContainer>
-            <BigCalendar
-                localizer={localizer}
-                events={events}
-                startAccessor="start"
-                endAccessor="end"
-                style={{ height: 600, width: 1000 }}
-                BackgroundWrapper="red"
-                onSelectEvent={(e)=>{modalHandler(e.val)}}
-                eventPropGetter={(myEvent) => {
-                const backgroundColor = myEvent.colorEvento ? myEvent.colorEvento : 'blue';
-                const color = myEvent.color ? myEvent.color : 'blue';
-                return { style: { backgroundColor, color } };
-                }}
-            />
+    <div className='flex flex-col  items-center p-3 gap-3'>
+      <div className='bg-white p-1 rounded-[5px]'>
+        <Selector value={formType} setValue={setFormType} list={[{ name: "Seminar" }, { name: "Guest House" }]} />
+      </div>
+      <CalendarContainer>
 
-            {user === "user" ? 
-            
-                <UserDataModal
-                    isModalOpen={isModalOpen}
-                    handleModalClose={handleModalClose}
-                    selectedRow={selectedRow}
-                    fetchData={()=>{}}
-                />
-                
-            :
-            
-                <AdminDataModal
-                    isModalOpen={isModalOpen}
-                    handleModalClose={handleModalClose}
-                    selectedRow={selectedRow}
-                    fetchData={()=>{}}
-                />
-            
-            }
-            
-        </CalendarContainer>
+        <BigCalendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 700, width: 1400 }}
+          BackgroundWrapper="red"
+          onSelectEvent={(e) => { modalHandler(e.val) }}
+          eventPropGetter={(event) => {
+            const backgroundColor = event.colorEvento ? event.colorEvento : 'blue';
+            const color = event.color ? event.color : 'blue';
+
+            // Customize the event title to include start and end times
+            const eventTitle = `${event.title} (${formatTime(event.start)} - ${formatTime(event.end)})`;
+
+            return {
+              style: { backgroundColor, color },
+              title: eventTitle, // Set the customized event title
+            };
+          }}
+        />
+
+        {user === "user" ?
+
+          <UserDataModal
+            isModalOpen={isModalOpen}
+            handleModalClose={handleModalClose}
+            selectedRow={selectedRow}
+            fetchData={() => { }}
+          />
+
+          :
+
+          <AdminDataModal
+            isModalOpen={isModalOpen}
+            handleModalClose={handleModalClose}
+            selectedRow={selectedRow}
+            fetchData={() => { }}
+          />
+
+        }
+
+      </CalendarContainer>
     </div>
   );
 };
